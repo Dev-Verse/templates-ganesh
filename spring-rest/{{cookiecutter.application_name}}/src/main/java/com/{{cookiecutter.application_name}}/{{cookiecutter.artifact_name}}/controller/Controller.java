d514381bffc61b1ccc4baba27a8040894bef3c1b
Aakash Rajput <aakash.rajput@techverito.com>
2023-07-11 15:33:48 +0530

    Adds yaml file as propery in spite of properties file

--- spring-rest/{{cookiecutter.application_name}}/src/main/java/com/{{cookiecutter.application_name}}/{{cookiecutter.artifact_name}}/controller/Controller.java
+++ spring-rest/{{cookiecutter.application_name}}/src/main/java/com/{{cookiecutter.application_name}}/{{cookiecutter.artifact_name}}/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.{{cookiecutter.application_name}}.{{cookiecutter.artifact_name}}.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @GetMapping("/")
     public String helloWorld(){
         return "Hello World !";
     }
